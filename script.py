93d1a80789f101c5ef1f4254bf714401f5729cf4
IOIIIO <101110@vivaldi.net>
2019-04-20 16:37:39 +0200

    stuff

--- script.py
+++ script.py
@@ -5,14 +5,15 @@ class tool:
     def __init__(self):
         self.plist = None
         self.dir = None
+        self.destination = None
+        self.destination_data = None
         self.u = utils.Utils("PatchPorter")
 
     def menu(self):
         self.u.head()
         print("")
         print("1. Select patches.plist")
-        print("2. Select result.plist directory")
-        print("3. Port patches")
+        print("2. Port patches")
         print("")
         print("Q. Quit")
         print("")
@@ -24,9 +25,21 @@ class tool:
         elif menu == "1":
             self._get_plist()
         elif menu == "2":
-            self._get_directory()
-        elif menu == "4":
+            self._patch()
+        elif menu == "3":
             self._debug()
+        elif menu == "4":
+            self._write_test()
+
+    def _patch(self):
+        self.u.head("Porting Patches!")
+        print("")
+        print("This may take a while.")
+        print("")
+
+    def _write_test(self):
+        
+
 
     def _get_plist(self):
         self.u.head("Select Plist")
@@ -58,44 +71,6 @@ class tool:
         try:
             with open(pc, "rb") as f:
                 self.plist = p
-                self.plist_data = plist.load(f)
-        except Exception as e:
-            self.u.head("Plist Malformed")
-            print("")
-            print("Plist file malformed:\n\n{}".format(e))
-            print("")
-            self.u.grab("Press [enter] to return...")
-            self._get_plist()
-
-    def _get_directory(self):
-        self.u.head("Select Directory")
-        print("")
-        print("Current: {}".format(self.dir))
-        print("")
-        print("C. Clear Selection")
-        print("M. Main Menu")
-        print("Q. Quit")
-        print("")
-        p = self.u.grab("Please drag and drop the target plist:  ")
-        if p.lower() == "q":
-            self.u.custom_quit()
-        elif p.lower() == "m":
-            return
-        elif p.lower() == "c":
-            self.plist = None
-            self.plist_data = None
-            return
-        
-        pc = self.u.check_path(p)
-        if not pc:
-            self.u.head("File Missing")
-            print("")
-            print("Plist file not found:\n\n{}".format(p))
-            print("")
-            self.u.grab("Press [enter] to return...")
-            self._get_plist()
-        try:
-            with open(pc, "rb") as f:
                 self.plist_data = plist.load(f)
         except Exception as e:
             self.u.head("Plist Malformed")
